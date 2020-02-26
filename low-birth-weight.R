343d362d4f14c00a1c472f154f1bc8a87d3fdd61
mmzielonka <61316633+mmzielonka@users.noreply.github.com>
2020-02-26 16:47:55 +0100

    Update low-birth-weight.R

--- low-birth-weight.R
+++ low-birth-weight.R
@@ -3,3 +3,25 @@
 
 birth.table <- read.table('lowbwt.dat')
 
+###PART I (magda /// 26.02.2019)
+#starting model with main effects and interactions terms we hypothesize might be relevant
+summary(glm(LOW ~ AGE + LWT +FTV + RACE + LWT*FTV + AGE*FTV, 
+            data=lowbwt.small, 
+            family=binomial(link="logit")))
+#Residual deviance: 211.99 
+
+#remove LWT*FTV - high p-value
+summary(glm(LOW ~ AGE + LWT +FTV + RACE + AGE*FTV,  
+            data=lowbwt.small, 
+            family=binomial(link="logit")))
+#Residual deviance: 212.62
+
+#remove RACE - high p-value (age is higher but we can't remove it if we can't to keep AGE*FTV which appears to be very significant)
+summary(glm(LOW ~ AGE + LWT +FTV + AGE*FTV , 
+            data=lowbwt.small, 
+            family=binomial(link="logit")))
+#Residual deviance: 215.37
+#these are all significant 
+
+##technically residual deviance is going up as we remove terms - we can test using AIC to see that removing these terms does improve the model 
+##BUT how is this different from the backwards selection we have to do in part 2 of analysis 1?
